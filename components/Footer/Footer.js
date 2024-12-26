de5adb03c0d7bec4f614f91b045f9553c6c1c855
Tasnimul Haque <tasnimul.haque6@gmail.com>
2024-12-26 13:22:48 +0600

    Update Footer component to include developer credit and adjust spacing

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -6,12 +6,19 @@ const Footer = () => {
   return (
     <footer className="!bg-white rounded-lg shadow m-4 dark:bg-gray-800 z-50">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 flex items-center justify-center gap-2">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 flex items-center justify-center gap-1">
           © {currentYear}
           <Link href="/" className="hover:underline">
             BlogLume.
           </Link>
-          All Rights Reserved.
+          All Rights Reserved. Developed by
+          <Link
+            href="https://tasnimul.vercel.app/"
+            target="_blank"
+            className=""
+          >
+            Tasnimul Haque
+          </Link>
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
