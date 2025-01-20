ce84160b1aa08432e8e3dfba7f44c603ef8148e9
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-20 23:27:28 +0600

    Update Hero component to use a local image source for improved loading performance

--- components/HomeComponents/Hero.js
+++ components/HomeComponents/Hero.js
@@ -90,7 +90,7 @@ const Hero = async () => {
                           width={40}
                           height={40}
                           alt=""
-                          src={`https://plus.unsplash.com/premium_photo-1689977927774-401b12d137d6?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`}
+                          src={`/TasnimulHaque-.webp`}
                           className="size-7 rounded-full bg-gray-50 object-cover"
                         />
                         <div className="text-sm/6">
