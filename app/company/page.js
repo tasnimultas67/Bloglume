2516f0b853caa9a12dda6c4f0ef3131ba6a5f9cd
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-21 11:20:25 +0600

    Enhance About section in Company page with additional content and improved layout for better user engagement

--- app/company/page.js
+++ app/company/page.js
@@ -10,15 +10,15 @@ export const metadata = {
 const page = () => {
   return (
     <div className=" m-auto py-20">
-      <div className="flex flex-col space-y-10">
+      <div className="flex flex-col space-y-20 ">
         <div className="flex flex-col items-center space-y-2 md:space-y-4">
           <h1 className="text-5xl md:text-7xl font-bold">BlogLume</h1>
           <p className="text-gray-500">Illuminating the world with news</p>
-        </div>
-        {/* Featured Image */}
-        <div>
+
+          {/* Featured Image */}
+
           <Image
-            className="w-full h-[20rem] md:h-[30rem] object-cover object-center"
+            className="w-11/12 m-auto rounded-2xl h-[20rem] md:h-[30rem] object-cover object-center"
             src="https://images.unsplash.com/photo-1557113166-6bf8102e535b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             width={500}
             height={500}
@@ -26,7 +26,7 @@ const page = () => {
           ></Image>
         </div>
         {/* About Company */}
-        <div className="flex flex-col justify-center w-11/12 md:w-10/12 m-auto">
+        <div className="flex flex-col justify-center w-11/12 md:w-10/12 m-auto space-y-10 text-center">
           <p className="text-center text-lg md:text-2xl">
             Your go-to destination for the latest news and insightful blogs. At
             BlogLume, we illuminate every corner of the world with our
@@ -34,6 +34,36 @@ const page = () => {
             and inspired with our diverse range of topics, from current events
             to lifestyle tips.
           </p>
+          <p>
+            At BlogLume, we believe in the power of words to illuminate the
+            world. Our mission is to bring you the latest news and insightful
+            blogs that not only inform but also inspire. We cover a diverse
+            range of topics, from current events to lifestyle tips, ensuring
+            there's something for everyone.
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 w-11/12 m-auto items-center py-10">
+          <div>
+            <Image
+              className="rounded-2xl h-[20rem] md:h-[30rem] object-cover object-center"
+              src="https://images.unsplash.com/photo-1712431104228-6fb10e02e7b6?q=80&w=1952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              width={500}
+              height={700}
+              alt="About Us - BlogLume"
+            ></Image>
+          </div>
+          <div className="space-y-5">
+            <h3 className="text-2xl font-semibold tracking-tight">Our Story</h3>
+            <p>
+              BlogLume was born out of a passion for storytelling and a desire
+              to create a platform where voices from all walks of life could be
+              heard. We started as a small team of enthusiastic writers and
+              journalists, and over the years, we've grown into a vibrant
+              community of contributors and readers. Our journey has been fueled
+              by our commitment to quality content and our love for sharing
+              stories that matter.
+            </p>
+          </div>
         </div>
       </div>
     </div>
