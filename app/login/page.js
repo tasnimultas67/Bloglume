ab06bdeea95f08219f346a09d97efb0c93fbf00e
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-21 10:01:34 +0600

    Refactor login page layout by removing unnecessary vertical padding in the login form

--- app/login/page.js
+++ app/login/page.js
@@ -16,9 +16,9 @@ export default function page() {
           />
         </div>
         {/* Login Form */}
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+            <h2 className=" text-center text-2xl/9 font-bold tracking-tight text-gray-900">
               Sign in to your account
             </h2>
           </div>
